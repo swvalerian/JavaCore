640b0c328a77e1c4fae509ff11e34b1e7ec3e551
Valeriy Panafidin <swvalerianJava@yandex.ru>
2021-02-20 21:44:48 +0500

    First commit add! URA

---
+++ src/main/java/com/swvalerian/javacore/chapter1/Main.java
@@ -0,0 +1,6 @@
+package com.swvalerian.javacore.chapter1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, это моя первая программа в репозитории");
+}
