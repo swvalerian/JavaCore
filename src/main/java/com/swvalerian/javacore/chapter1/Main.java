4b703aa3878e2a1896aec38abc3669c71dfe2a23
Valeriy Panafidin <swvalerianJava@yandex.ru>
2021-02-20 22:03:51 +0500

    Second programm add

--- src/main/java/com/swvalerian/javacore/chapter1/Main.java
+++ src/main/java/com/swvalerian/javacore/chapter1/Main.java
@@ -3,4 +3,5 @@ package com.swvalerian.javacore.chapter1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, это моя первая программа в репозитории");
+    }
 }
---
+++ src/main/java/com/swvalerian/javacore/chapter1/Second.java
@@ -0,0 +1,7 @@
+package com.swvalerian.javacore.chapter1;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Поздравляю, вторая программа добавлена!");
+    }
+}
